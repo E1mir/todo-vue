1d0cc145cdf4a28368ff4adb045c2dfc7d3be4b3
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-05-29 15:58:24 +0200

    conf: add eslint, prettier, stylelint, husky, lint-staged, sass configurations

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-list">TODO</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .todo-list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/todo-vue/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ``,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [vue()],
 })
